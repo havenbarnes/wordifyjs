c83797df5890dca546a3b04c3b70e9a39e0e666d
Haven Barnes <haven38@gmail.com>
2019-10-31 02:36:20 -0700

    feat(): MVP word/uuid generation and translation finished

--- src/index.js
+++ src/index.js
@@ -1,24 +1,45 @@
-import { wordForIndex } from './words';
+import { wordForIndex, indexForWord } from './words';
 import uuidv4 from 'uuid/v4';
-
-const generateWords = () => {
-	return 'asdf';
-};
-
-const generateUUID = () => {
-	return 'asdf';
-};
 
 const generateNewPair = () => {
 	const uuid = uuidv4();
-	const words = [];
-	const segmentLength = 4;
-	for (var i = 0; i < uuid.length - segmentLength; i += segmentLength) {
-		const segment = uuid.substring(i, i + segmentLength).replace('-', '1');
-		const wordIndex = parseInt(segment, 16);
-		words.push(wordForIndex(wordIndex));
-	}
-	return [uuid, words.join('-')];
+	return [uuid, wordsFromUUID(uuid)];
 };
 
-export { generateWords, generateUUID, generateNewPair };
+const getIntegerFromHex = hex => {
+	return parseInt(hex, 16);
+};
+
+const getHexFromInteger = integer => {
+	let hex = Number(integer).toString(16);
+	return hex;
+};
+
+const wordsFromUUID = uuid => {
+	const uuidNoDash = uuid.replace(/-/g, '');
+	const words = [];
+	const segmentLength = 4;
+	for (var i = 0; i < uuidNoDash.length; i += segmentLength) {
+		const segment = uuidNoDash.substring(i, i + segmentLength);
+		const wordIndex = getIntegerFromHex(segment);
+		words.push(wordForIndex(wordIndex));
+	}
+	return words.join('-');
+};
+
+const uuidFromWords = wordUUID => {
+	const words = wordUUID.split('-');
+	let uuid = '';
+	words.forEach((word, index) => {
+		const wordIndex = indexForWord(word);
+		const hex = getHexFromInteger(wordIndex);
+		uuid += hex;
+		const dashIndexes = [1, 2, 3, 4];
+		if (dashIndexes.includes(index)) {
+			uuid += '-';
+		}
+	});
+	return uuid;
+};
+
+export { generateNewPair, wordsFromUUID, uuidFromWords };
