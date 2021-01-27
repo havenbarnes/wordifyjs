bf867d14d7eafe0888ac67cf462c96db4309f235
Haven Barnes <haven38@gmail.com>
2021-01-26 20:45:18 -0800

    Update dependencies, fix tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,16 +4,10 @@ beforeEach(() => {
 	// Set UUID seed
 });
 
-test('try test', () => {
-	const pair = ['92bc740d-ccc9-45dd-aea7-0ed2982588cb', 'checklisted-bumptious-cretinised-barbels-commemoration-aftergrass-chocoholic-cathedra'];
-	const translated = wordify.uuidFromWords(pair[1]);
-	expect(translated).toBe(pair[0]);
+test('UUID can be translated to and from word ID', () => {
+	for (var i = 0; i < 100; i++) {
+		const pair = wordify.generateNewPair();
+		const translated = wordify.uuidFromWords(pair[1]);
+		expect(translated).toBe(pair[0]);
+	}
 });
-
-// test('UUID can be translated to and from word ID', () => {
-// 	for (var i = 0; i < 100; i++) {
-// 		const pair = wordify.generateNewPair();
-// 		const translated = wordify.uuidFromWords(pair[1]);
-// 		expect(translated).toBe(pair[0]);
-// 	}
-// });
